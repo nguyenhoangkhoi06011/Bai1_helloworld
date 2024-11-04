0e6fc7492f32871e607a7961f77c318d072815a5
nguyenhoangkhoi06011 <nguyenhoangkhoi012021@gmail.com>
2024-11-04 11:23:47 +0700

    Add project files.

---
+++ Bai1_helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Hello World!\n";
+	cout << "Toi ten la_" << "Nguyen Hoang Khoi" << endl;
+	cout << "Toi dang hoc\tC++" << endl;
+	return 0;
+}
